7bad43fb0c8c7fc72dd298aec8e6273d3395c3d8
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-02-10 17:25:45 +0000

    red: return_true_when_year_is_divisible_by_400

--- KataLeapYear/KataLeapYear/UnitTest1.cs
+++ KataLeapYear/KataLeapYear/UnitTest1.cs
@@ -8,9 +8,7 @@ namespace KataLeapYear {
 
         [Test]
         public void return_true_when_year_is_divisible_by_400() {
-            bool result = false;
-
-            result = LeapYear.IsLeap(800);
+            var result = LeapYear.IsLeap(800);
 
             Assert.AreEqual(true, result);
         }
