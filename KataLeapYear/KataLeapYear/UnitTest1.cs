8a8722e4dc2d2f2f9992cf7579078e4ac9e37910
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-02-11 12:15:01 +0000

    green: return_true_when_year_is_divisible_by_4_and_not_by_100

--- KataLeapYear/KataLeapYear/UnitTest1.cs
+++ KataLeapYear/KataLeapYear/UnitTest1.cs
@@ -32,6 +32,7 @@ namespace KataLeapYear {
 
     public static class LeapYear {
         public static bool IsLeap(int year) {
+            if ((year % 4) == 0 && (year % 100) != 0) return true;
             return year % 400 == 0;
         }
     }
